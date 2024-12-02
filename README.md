73ca94992e5a1dcd44bf97f9ed011c1f960dede6
rached <rachedgalaxy@gmail.com>
2024-12-02 11:12:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+<h1 align="center" id="title">Portfolio Website V2</h1>
+
+<p id="description">A user-friendly portfolio website showcasing my projects skills and providing a way for others to get in touch. Made using Vite.Js and Three.Js.</p>
+
+<h2>🚀 Demo</h2>
+
+[https://portfolio-lohit.vercel.app/](https://portfolio-lohit.vercel.app/)
+
+  
+  
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+*   Home page with a hero section welcoming visitors and providing a brief introduction.
+*   About page highlighting my background skills and experience.
+*   Work page showcasing my projects with descriptions screenshots and links.
+*   Contact page with a contact form for visitors to reach out to me.
+*   Resume page providing a downloadable version of my resume.
+*   Responsive design for a great user experience on different devices.
+
+<h2>🛠️ Installation Steps:</h2>
+
+<p>1. Clone the repository:</p>
+
+```
+git clone https://github.com/lohitkolluri/Portfolio-Website.git
+```
+
+<p>2. Navigate to directory:</p>
+
+```
+cd Portfolio-Website
+```
+
+<p>3. Install dependencies</p>
+
+```
+ npm install
+```
+
+<p>4. Start the development server:</p>
+
+```
+ npm run dev
+```
+
+<p>5. The website will be accessible at</p>
+
+http://localhost:3000
+  
+  
+<h2>💻 Built with</h2>
+
+Technologies used in the project:
+
+*   EmailJS
+*   Vite.Js
+*   ThreeJs
+*   Tailwind CSS
+
+<h2>🛡️ License:</h2>
+
+This project is licensed under the [MIT License](LICENSE)
---
+++ src/constants/index.js
@@ -0,0 +1,145 @@
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  { title: "C", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech-Stack/C.png" },
+  { title: "C++", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech-Stack/CPP.png" },
+  { title: "Python", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech-Stack/Python.png" },
+  { title: "Java", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech-Stack/Java.png" },
+];
+
+export const technologies = [
+  { name: "HTML 5", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/HTML.png" },
+  { name: "CSS 3", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/CSS.png" },
+  { name: "JavaScript", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/Javascript.png" },
+  { name: "Rect JS", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/ReactJS.png" },
+  { name: "Tailwind CSS", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/TailwindCSS.png" },
+  { name: "Node JS", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/NodeJS.png" },
+  { name: "Three JS", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/ThreeJS.png" },
+  { name: "git", icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Tech/Git.png" },
+];
+
+export const experiences = [
+  {
+    title: "AI/ML Intern",
+    company_name: "EduSkill Foundation | AWS Academy | AICTE",
+    icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Experiences/EduSkill.png",
+    iconBg: "#161329",
+    date: "Sep 2023 - Nov 2023",
+    points: [
+      "Acquired hands-on knowledge of AWS Cloud Foundation, delving into cloud infrastructure, services, and deployment.",
+      "also gaining a solid foundation in Machine Learning, covering topics like algorithms, data analysis, and model building.",
+    ],
+  },
+  {
+    title: "Mathwork Ai Virtual Intern",
+    company_name: "Mathwork | AICTE",
+    icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Experiences/MathWorks.png",
+    iconBg: "#161329",
+    date: "May 2023 - Sep 2023",
+    points: [
+      "Completed virtual internship, gaining a strong foundation in MATLAB, including data analysis and processing.",
+      "Acquired practical skills in image and signal processing, including segmentation, batch processing, and spectral analysis.",
+      "Developed expertise in machine learning models for clustering, classification, and regression, and customized deep learning techniques for image classification.",
+    ],
+  },
+  {
+    title: "Artificial Intelligence Intern",
+    company_name: "Edunet Foundation | IBM SkillsBuild | AICTE",
+    icon: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Experiences/Edunet.png",
+    iconBg: "#161329",
+    date: "June 2023 - July 2023",
+    points: [
+      "Engineered a comprehensive Mental Health Fitness Tracker ML model utilizing Python and scikit-learn.",
+      "Maximized the model's performance by refining model parameters and employing ensemble methods, yielding an outstanding accuracy percentage of 98.50%.",
+      "Leveraged 12 regression algorithms to attain precise outcomes in analyzing and predicting mental fitness levels across 150+ countries.",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    name: "WeatherPedia",
+    description:
+      "Web-based platform that allows users to access weather information for their location by entering it in the search field",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "HTML", color: "green-text-gradient" },
+      { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
+      { name: "Weather API by API Ninjas", color: "yellow-text-gradient" },
+    ],
+    image: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Projects/WeatherPedia.png",
+    source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
+  },
+  {
+    name: "Terminal Like Portfolio Website",
+    description:
+      "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
+    tags: [
+      { name: "HTML", color: "blue-text-gradient" },
+      { name: "css", color: "green-text-gradient" },
+      { name: "Javascript", color: "pink-text-gradient" },
+    ],
+    image: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Projects/TerminalPW.png",
+    source_code_link: "https://github.com/lohitkolluri/lohitkolluri.github.io",
+  },
+  {
+    name: "Mental Health Fitness Tracker",
+    description:
+      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Jupyter Notebook", color: "green-text-gradient" },
+      { name: "Regression Algorithms", color: "pink-text-gradient" },
+    ],
+    image: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Projects/MHFT.png",
+    source_code_link:
+      "https://github.com/lohitkolluri/mental_health_fitness_tracker",
+  },
+  {
+    name: "PayloadMaster",
+    description:
+      "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "shell", color: "blue-text-gradient" },
+    ],
+    image: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Projects/PayloadMaster.png",
+    source_code_link: "https://github.com/lohitkolluri/PayloadMaster",
+  },
+  {
+    name: "CompileVortex",
+    description:
+      "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "CSS", color: "green-text-gradient" },
+      { name: "HTML", color: "pink-text-gradient" },
+    ],
+    image: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Projects/CompileVortex.png",
+    source_code_link: "https://github.com/lohitkolluri/CompileVortex",
+  },
+  {
+    name: "Sketcher",
+    description:
+      "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
+    tags: [
+      { name: "OpenCV", color: "blue-text-gradient" },
+      { name: "Matplotlib", color: "green-text-gradient" },
+      { name: "Python", color: "pink-text-gradient" },
+    ],
+    image: "https://lohitcdn.blob.core.windows.net/portfoliocdn/Projects/Sketcher.png", 
+    source_code_link: "https://github.com/lohitkolluri/SketchiFy",
+  },
+];
